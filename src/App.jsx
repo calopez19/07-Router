995597bfb5134eb6c14e4a3d1c6bd26f5e479296
camiloUbuntu <53837383+calopez19@users.noreply.github.com>
2025-12-14 20:46:16 -0300

    Se creo una forma para  realizar MPA

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function HomePage() {
   return (
     <>
-      <h1>Router</h1>
+      <h1>Home</h1>
+      <p>Esta es una pagina de ejemplo para crear un React Router</p>
+      <a href="/about">Sobre nosotros</a>
     </>
-  )
+  );
 }
 
-export default App
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Hola este es un clon de React Routerr con fines pedagogicos </p>
+      <a href="/">Ir a home</a>
+    </>
+  );
+}
+function App() {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  return (
+    <main>
+      {currentPath === '/' && <HomePage />}
+      {currentPath === '/about' && <AboutPage />}
+    </main>
+  );
+}
+
+export default App;
