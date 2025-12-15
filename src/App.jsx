db7220c457bd02020a978e706a79440443ffe436
camiloUbuntu <53837383+calopez19@users.noreply.github.com>
2025-12-15 10:53:12 -0300

    creada la suscripcion al evento Navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+const NAVIGATION_EVENT = "pushstate";
+
+//Esta funcion va a cambiar la url, crea el evento de navegacion y lo despacha para que otra funcion pueda escuchar el evento
+function navigate(href) {
+  // El metodo de abajo permite cambiar el link de la pagina con el parametro dado sin recargar la pagina
+  window.history.pushState({}, "", href);
+  //ahora se va a crear un evanto personalizado para avisar que se a cambiado la url
+  const navigationEvent = new Event(NAVIGATION_EVENT);
+  // lo llamamos pushState porque no hay una forma para escuchar el window.history.pushState ( si lo hay cuando uno navega hacia atras)
+  window.dispatchEvent(navigationEvent);
+}
 function HomePage() {
   return (
     <>
       <h1>Home</h1>
       <p>Esta es una pagina de ejemplo para crear un React Router</p>
-      <a href="/about">Sobre nosotros</a>
+      <button
+        onClick={() => {
+          navigate("/about");
+        }}
+      >
+        Sobre nosotros
+      </button>
     </>
   );
 }
@@ -18,17 +35,33 @@ function AboutPage() {
     <>
       <h1>About</h1>
       <p>Hola este es un clon de React Routerr con fines pedagogicos </p>
-      <a href="/">Ir a home</a>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Ir a home
+      </button>
     </>
   );
 }
 function App() {
   const [currentPath, setCurrentPath] = useState(window.location.pathname);
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname);
+    };
 
+    window.addEventListener(NAVIGATION_EVENT, onLocationChange);
+
+    return () => {
+      window.removeEventListener(NAVIGATION_EVENT, onLocationChange);
+    };
+  }, []);
   return (
     <main>
-      {currentPath === '/' && <HomePage />}
-      {currentPath === '/about' && <AboutPage />}
+      {currentPath === "/" && <HomePage />}
+      {currentPath === "/about" && <AboutPage />}
     </main>
   );
 }
